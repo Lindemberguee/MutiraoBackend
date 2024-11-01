c239ec187606124f26130fb78d3c5ecfa42e64be
José <61948366+Lindemberguee@users.noreply.github.com>
2024-11-01 13:54:50 -0300

    Update anamneseController.js

--- controllers/anamneseController.js
+++ controllers/anamneseController.js
@@ -1,4 +1,5 @@
 const Anamnese = require('../models/Anamnese');
+const moment = require('moment'); // Moment.js para manipulação de datas
 
 // CREATE - Criar uma nova anamnese
 exports.createAnamnese = async (req, res) => {
@@ -28,13 +29,32 @@ exports.getAnamneseById = async (req, res) => {
 // READ - Listar todas as anamneses
 exports.getAllAnamneses = async (req, res) => {
   try {
-    const anamneses = await Anamnese.find();
+    const { especialidade, ano, idunico } = req.query;
+    const filter = {};
+
+    // Filtrar por especialidade
+    if (especialidade) filter['anamnese.especialidade'] = especialidade;
+
+    // Filtrar por ano de dataAtendimento
+    if (ano) {
+      // Usando regex para encontrar qualquer data que contenha o ano especificado
+      const anoRegex = new RegExp(`/${ano}`, 'g');
+      filter['anamnese.dataAtendimento'] = { $regex: anoRegex };
+    }
+
+    // Filtrar por ID único
+    if (idunico) filter.idunico = idunico;
+
+    // Consultar as anamneses usando os filtros
+    const anamneses = await Anamnese.find(filter);
     res.status(200).json({ success: true, data: anamneses });
   } catch (err) {
     console.error('Erro ao listar todas as anamneses:', err.message);
     res.status(400).json({ success: false, error: err.message });
   }
 };
+
+
 
 // UPDATE - Atualizar os detalhes de uma anamnese existente
 exports.updateAnamnese = async (req, res) => {
